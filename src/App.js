298a1973564c570e09b57b6767044a4d67ebdaa1
cpatelss90 <charvin.patel@smoothstack.com>
2021-05-13 21:59:01 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import AuthService from "./services/authService";
+
+import Login from "./components/loginComponent";
+import Profile from "./components/profileComponent";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+
+    this.state = {
+      currentUser: undefined,
+    };
+
+  }
+
+  componentDidMount() {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      this.setState({
+        currentUser: user,
+      });
+    }
+  }
+
+  logOut() {
+    AuthService.logout();
+  }
+
+  render() {
+    const { currentUser} = this.state;
+
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <div className="navbar-nav mr-auto">
+          </div>
+
+          {currentUser ? (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/profile"} className="nav-link">
+                  {currentUser.username}
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link" onClick={this.logOut}>
+                  LogOut
+                </a>
+              </li>
+            </div>
+          ) : (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/login"} className="nav-link">
+                  Login
+                </Link>
+              </li>
+            </div>
+          )}
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/profile" component={Profile} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/profileComponent.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import AuthService from "../services/authService";
+
+export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      redirect: null,
+      userReady: false,
+      currentUser: { username: "" }
+    };
+  }
+
+  componentDidMount() {
+    const currentUser = AuthService.getCurrentUser();
+
+    if (!currentUser) this.setState({ redirect: "/home" });
+    this.setState({ currentUser: currentUser, userReady: true })
+  }
+
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to={this.state.redirect} />
+    }
+
+    const { currentUser } = this.state;
+
+    return (
+      <div className="container">
+        {(this.state.userReady) ?
+        <div>
+        <header className="jumbotron">
+          <h3>
+            <strong> Hello {currentUser.username}!</strong>
+          </h3>
+        </header>
+        <p>
+          <strong>Token:</strong>{" "}
+          {currentUser.accessToken.substring(0, 20)} ...{" "}
+          {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
+        </p>
+        <p>
+          <strong>Id:</strong>{" "}
+          {currentUser.id}
+        </p>
+        <p>
+          <strong>User Name:</strong>{" "}
+          {currentUser.username}
+        </p>
+        <p>
+          <strong>Email:</strong>{" "}
+          {currentUser.email}
+        </p>
+        <p>
+          <strong>Phone Num:</strong>{" "}
+          {currentUser.phNum}
+        </p>
+        <p>
+          <strong>First Name:</strong>{" "}
+          {currentUser.firstName}
+        </p>
+        <p>
+          <strong>Last Name:</strong>{" "}
+          {currentUser.lastName}
+        </p>
+      </div>: null}
+      </div>
+    );
+  }
+}
