7687b772790a64c3899d9bc1192dee6482793131
Josten Asercion <josten.asercion@smoothstack.com>
2021-05-26 11:38:15 -0400

    View cards on offer front end complete

--- src/components/cardsOnOfferComponent.js
+++ src/components/cardsOnOfferComponent.js
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function CardsOnOfferComponent() {
-    return (
-        <div>
-            These are cards we have.
-        </div>
-    )
+import cardimage from '../assets/Card.png';
+
+
+export default class CardsOnOfferComponent extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div class="row">
+                <h3 class = "text-center">Available Cards</h3>
+                <div className="col">
+                    <div className="card card-container">
+                        <h3 class = "text-center">Premium Card</h3>
+                        <img src={cardimage} />
+                        <hr></hr>
+                        Cashmoney Premium card with low interest and stuff.
+                        <hr></hr>
+                        <a class="btn btn-primary" href="#" role="button">Sign Up</a>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card card-container">
+                        <h3 class = "text-center">Next Card</h3>
+                        <img src={cardimage} />
+                        <hr></hr>
+                        Cashmoney Next card with things.
+                        <hr></hr>
+                        <a class="btn btn-primary" href="#" role="button">Sign Up</a>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card card-container">
+                        <h3 class = "text-center">Other Card</h3>
+                        <img src={cardimage} />
+                        <hr></hr>
+                        Cashmoney Other card with other things.
+                        <hr></hr>
+                        <a class="btn btn-primary" href="#" role="button">Sign Up</a>
+                    </div>
+                </div>
+
+            </div>
+            
+
+        );
+    }
+
 }
+
