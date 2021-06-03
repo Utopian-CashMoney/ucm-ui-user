a911caa524b416f0672136981325622910a678a0
cpatelss90 <charvin.patel@smoothstack.com>
2021-06-03 17:51:44 -0500

    User Profile Story

--- src/components/LoansOnOfferComponent.js
+++ src/components/LoansOnOfferComponent.js
@@ -3,6 +3,9 @@ import {DashboardLayout} from './Layout';
 
 
  import house from '../house.jpg';
+ import car from '../car.jpg'
+ import personal from '../personal.jpg'
+
 
 
 export default class LoansOnOfferComponent extends Component {
@@ -15,33 +18,43 @@ export default class LoansOnOfferComponent extends Component {
             <DashboardLayout>
             <div class="row">
                 <h3 class = "text-center">Available Loans</h3>
-                <div className="col">
+                <div className="row">
                     <div className="card card-container">
                         <h3 class = "text-center">Home Loan</h3>
                         <img src={house} />
                         <hr></hr>
-                        Cashmoney Premium card with low interest and stuff.
+                        <h5>Cashmoney Home Loan for Purchasing new House:</h5>
                         <hr></hr>
+                        <h7>* APR (Annual Percentage Rate): 2.5%, 3.7%, 4.5%, 5.6% (Based on Terms)</h7>
+                        <h7>* Mortgage Terms Available: 5, 10, 20, 30 Years</h7>
+                        <h7>* Down Payment required: 10%</h7>
+                        
                         <a class="btn btn-primary" href="#" role="button">Sign Up</a>
                     </div>
                 </div>
                 <div className="row">
                     <div className="card card-container">
                         <h3 class = "text-center">Auto Loan</h3>
-                        {/* <img src={cardimage} /> */}
+                        <img src={car} />
                         <hr></hr>
-                        Cashmoney Next card with things.
+                        <h5>Cashmoney Auto Loan for Purchasing New/used Cars:</h5>
                         <hr></hr>
+                        <h7>APR (Annual Percentage Rate): 3.5%, 3.7%, 4.1%, 4.7% (Based on Terms) </h7>
+                        <h7>Terms Available: 24, 36, 48, 60 Months</h7>
+                        <h7>Down Payment required: 5%</h7>
                         <a class="btn btn-primary" href="#" role="button">Sign Up</a>
                     </div>
                 </div>
                 <div className="row">
                     <div className="card card-container">
                         <h3 class = "text-center">Personal Loan</h3>
-                        {/* <img src={cardimage} /> */}
+                        <img src={personal} />
                         <hr></hr>
-                        Cashmoney Other card with other things.
+                        <h5>Cashmoney Personal Loan for family needs:</h5>
                         <hr></hr>
+                        <h7>APR (Annual Percentage Rate): 4.1%, 5.9%, 7.4%, 8.8% (Based on Terms)</h7>
+                        <h7>Mortgage Terms Available: 1, 3, 5, 10 Years</h7>
+                        <h7>Down Payment required: None</h7>
                         <a class="btn btn-primary" href="#" role="button">Sign Up</a>
                     </div>
                 </div>
